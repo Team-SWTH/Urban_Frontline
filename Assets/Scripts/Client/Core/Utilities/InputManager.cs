058b3da1e6a66332559ae0f6a2d331a5d7f4fc69
이인환 <70061182+leeinhwan0421@users.noreply.github.com>
2024-12-19 20:07:14 +0900

    some fix

---
+++ Assets/Scripts/Client/Core/Utilities/InputManager.cs
@@ -0,0 +1,21 @@
+// ========================================
+// File: InputManager.cs
+// Created: 2024-12-19 19:56:32
+// Author: leeinhwan0421
+// ========================================
+
+using System.Collections.Generic;
+using UnityEngine;
+
+using UrbanFrontline.Common;
+
+namespace UrbanFrontline.Client.Core.Utilities
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
