d6e56c44793842b3f73220c6a01107fe9a1507f4
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-12-19 18:53:45 +0900

    feat : Singleton 추가

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,40 @@
+// ========================================
+// File: Singleton.cs
+// Created: 2024-12-19 18:32:38
+// Author: ※ 작성자 이름을 반드시 기입해주세요.
+// ========================================using UnityEngine;
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UrbanFrameworks.Common
+{
+    public static class Singleton<TInstance> where TInstance : MonoBehaviour
+    {
+        private static Dictionary<Type, TInstance> m_instanceBank = new();
+        private static readonly object m_lock = new();
+
+        public static TInstance GetInstance()
+        {
+            lock (m_lock)
+            {
+                if (!m_instanceBank.ContainsKey(typeof(TInstance)) || 
+                    !m_instanceBank[typeof(TInstance)])
+                {
+                    TInstance instance = GameObject.FindObjectOfType<TInstance>();
+                    if (!instance)
+                    {
+                        // 새 GameObject 생성 및 TInstance 추가
+                        GameObject singletonObject = new($"{typeof(TInstance).Name}_Singleton");
+                        instance = singletonObject.AddComponent<TInstance>();
+                    }
+
+                    m_instanceBank[typeof(TInstance)] = instance;
+                }
+
+                return m_instanceBank[typeof(TInstance)];
+            }
+        }
+    }
+}
