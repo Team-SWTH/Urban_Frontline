7f87d2e4f7bf7346de8d7d28d844ae2a4897fcf4
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-12-19 19:01:33 +0900

    Update Singleton.cs
    - 주석 추가

--- Assets/Scripts/Common/Singleton.cs
+++ Assets/Scripts/Common/Singleton.cs
@@ -10,11 +10,27 @@ using UnityEngine;
 
 namespace UrbanFrameworks.Common
 {
+    /// <summary>
+    /// 싱글턴 패턴을 구현한 정적 클래스입니다.
+    /// 특정 MonoBehaviour 타입에 대한 전역적으로 접근 가능한 인스턴스를 제공합니다.
+    /// </summary>
+    /// <typeparam name="TInstance">MonoBehaviour를 상속하는 타입</typeparam>
     public static class Singleton<TInstance> where TInstance : MonoBehaviour
     {
+        /// <summary>
+        /// 타입별로 인스턴스를 저장하는 딕셔너리.
+        /// </summary>
         private static Dictionary<Type, TInstance> m_instanceBank = new();
+        
+        /// <summary>
+        /// Thread-Safe를 위한 Lock Object.
+        /// </summary>
         private static readonly object m_lock = new();
 
+        /// <summary>
+        /// 싱글턴 인스턴스를 가져옵니다.
+        /// </summary>
+        /// <returns>해당 타입의 싱글턴 인스턴스.</returns>
         public static TInstance GetInstance()
         {
             lock (m_lock)
@@ -25,8 +41,7 @@ namespace UrbanFrameworks.Common
                     TInstance instance = GameObject.FindObjectOfType<TInstance>();
                     if (!instance)
                     {
-                        // 새 GameObject 생성 및 TInstance 추가
-                        GameObject singletonObject = new($"{typeof(TInstance).Name}_Singleton");
+                        GameObject singletonObject = new($"{typeof(TInstance).Name}");
                         instance = singletonObject.AddComponent<TInstance>();
                     }
                     
