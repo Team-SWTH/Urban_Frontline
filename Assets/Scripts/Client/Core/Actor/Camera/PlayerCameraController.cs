4d7b2d246cd1d97716be498da55dd7b4647ed146
leeinhwan0421 <70061182+leeinhwan0421@users.noreply.github.com>
2025-01-02 15:04:21 +0900

    FreeLook 기능 복원, 구를 때 회전 제한 주는 것 구현
    - FreeLook 기능 복원
    - 구를 때 회전 제한 주는 것 구현

--- Assets/Scripts/Client/Core/Actor/Camera/PlayerCameraController.cs
+++ Assets/Scripts/Client/Core/Actor/Camera/PlayerCameraController.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Unity.Cinemachine;
+using Cysharp.Threading.Tasks;
 
 namespace UrbanFrontline.Client.Core.Actor.Camera
 {
@@ -129,6 +130,33 @@ namespace UrbanFrontline.Client.Core.Actor.Camera
         private float m_minMouseY = -70.0f;
         #endregion
 
+        [Space(10.0f)]
+        [Header("FreeLook Settings")]
+
+        #region FreeLook Settings
+        /// <summary>
+        /// 둘러보기 활성화 값
+        /// <summary>
+        [Tooltip("둘러보기 활성화 값")]
+        [SerializeField]
+        private bool m_freelookEnable;
+
+        /// <summary>
+        /// 둘러보기 비활성화 시 돌아갈 속도
+        /// </summary>
+        [Tooltip("둘러보기 비활성화 시 돌아갈 속도")]
+        [SerializeField]
+        private float m_freelookLerpSpeed = 5.0f;
+
+        /// <summary>
+        /// 둘러보기 비활성화 시 돌아갈 위치
+        /// </summary>
+        [Tooltip("둘러보기 비활성화 시 돌아갈 위치")]
+        [SerializeField]
+        private float m_beforeMouseX;
+        private float m_beforeMouseY;
+        #endregion
+
         private void Awake()
         {
             SetFieldOfView(m_fieldOfView);
@@ -142,9 +170,17 @@ namespace UrbanFrontline.Client.Core.Actor.Camera
             m_mouseY = Mathf.Clamp(m_mouseY, m_minMouseY, m_maxMouseY);
 
             Quaternion result = Quaternion.Euler(new Vector3(-m_mouseY, m_mouseX, 0));
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, result.eulerAngles.y, transform.rotation.eulerAngles.z);
-            m_cameraFollowTransform.rotation = Quaternion.Euler(result.eulerAngles.x, m_cameraFollowTransform.rotation.eulerAngles.y, result.eulerAngles.z);
-            m_rightHandTransform.rotation = Quaternion.LookRotation(m_cameraTransform.forward) * Quaternion.Euler(0, -90, 0);
+
+            if (m_freelookEnable == false)
+            {
+                m_cameraFollowTransform.rotation = Quaternion.Euler(result.eulerAngles.x, m_cameraFollowTransform.rotation.eulerAngles.y, result.eulerAngles.z);
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, result.eulerAngles.y, transform.rotation.eulerAngles.z);
+                m_rightHandTransform.rotation = Quaternion.LookRotation(m_cameraTransform.forward) * Quaternion.Euler(0, -90, 0);
+            }
+            else
+            {
+                m_cameraFollowTransform.rotation = Quaternion.Euler(result.eulerAngles.x, result.eulerAngles.y, result.eulerAngles.z);
+            }
         }
 
         private void LateUpdate()
@@ -170,6 +206,68 @@ namespace UrbanFrontline.Client.Core.Actor.Camera
         public void SetFieldOfView(float fieldOfView)
         {
             m_fieldOfView = fieldOfView;
+        }
+
+        /// <summary>
+        /// 둘러보기 감도(곱연산 값) 조정
+        /// </summary>
+        /// <param name="multipler">변경할 값</param>
+        public void SetSensitivityMultipler(float multipler)
+        {
+            m_sensitivityMultipler = multipler;
+        }
+
+        /// <summary>
+        /// FreeLook 상태 변환
+        /// </summary>
+        /// <param name="enable">freelook enable : true, disable : false</param>
+        public void SetFreeLookStatement(bool enable)
+        {
+            if (m_freelookEnable == false && enable == true)
+            {
+                m_beforeMouseX = m_mouseX;
+                m_beforeMouseY = m_mouseY;
+
+                m_freelookEnable = true;
+
+                return;
+            }
+            
+            if (m_freelookEnable == true && enable == false)
+            {
+                OffFreeLookAnimation().Forget();
+
+                return;
+            }
+        }
+
+        /// <summary>
+        /// FreeLook 비활성화 시 애니메이션
+        /// </summary>
+        /// <returns>void</returns>
+        private async UniTaskVoid OffFreeLookAnimation()
+        {
+            float beforeSensevity = m_sensitivity;
+            m_sensitivity = 0;
+
+            float t = 0.0f;
+
+            while (t < 1.0f)
+            {
+                t += Time.deltaTime * m_freelookLerpSpeed;
+
+                m_mouseX = Mathf.Lerp(m_mouseX, m_beforeMouseX, t);
+                m_mouseY = Mathf.Lerp(m_mouseY, m_beforeMouseY, t);
+
+                await UniTask.WaitForEndOfFrame();
+            }
+
+            m_mouseX = m_beforeMouseX;
+            m_mouseY = m_beforeMouseY;
+
+            m_sensitivity = beforeSensevity;
+
+            m_freelookEnable = false;
         }
     }
 }
